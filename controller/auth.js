ec843deea23e8a732e0f15336c0912dedb1c1a1d
Satya <sanee@satya.satya>
2025-08-25 16:49:26 +0530

    Some updates

--- controller/auth.js
+++ controller/auth.js
@@ -194,7 +194,9 @@ exports.signIn=async (req , res)=>{
       user.password=undefined;
       const options={
         expires:new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        httpOnly:true
+        httpOnly:true,
+        secure: true,          // ✅ required for cross-origin
+        sameSite: "None",     
       }
       res.cookie("token",token,options).status(200).json({
         success:true,
